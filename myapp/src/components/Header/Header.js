a607d1e3a5e74dfbe68820690d28e9aaa9739958
Mehmet Emin Tayfur <emin.tayfur2000@gmail.com>
2023-01-30 15:37:57 +0300

    30.01.23 15.37

--- myapp/src/components/Header/Header.js
+++ myapp/src/components/Header/Header.js
@@ -1,44 +1,32 @@
 import React from 'react'
 import { useState } from 'react';
-import { useContext } from 'react';
-import ListContext from '../../context/ListContext';
-import { Formik, Field, Form } from 'formik';
+
 
 
 function Header() {
     
-   // const name = value.
-    //const [form, useForm] = useState({name:"",});
+    const name = value.
+    const [form, useForm] = useState({name:"",});
 
-    //const onChangeInput = ()=>{               //<button onClick={() => {setList([...list,"sd"] ) }}> Add </button>
+    const onChangeInput = ()=>{               
 
-    //}
-
-    const [list,setList] = useContext(ListContext)
-    
-    function addToTheList(){
-        setList([...list,"asd"])
     }
+
+   
 
 
     return (
     <div>
-            <h3>  To Do List </h3>
-      <Formik
-      initialValues={{
-        addtask: '',
-      }}
-      onSubmit={async (values) => {
-        setList([...list,values.addtask])
-      }}
-    >
-       <Form>
-        <label htmlFor="addtask">Add a task</label>
-        <Field id="addtask" name="addtask" placeholder="Add a task" />
-
-        <button type="submit">Submit</button>
-        </Form>
-      </Formik>
+          <h3>  To Do List </h3>
+          <form onSubmit={onSubmit}>
+              <input
+                name='name'
+                value={form.name}
+                onChange={onChangeInput}
+                placeholder='Enter the task'>
+              </input>
+              <button> Add </button>
+            </form>
             
 
     </div>
--- myapp/src/components/Main.js
+++ myapp/src/components/Main.js
@@ -1,22 +1,22 @@
 import Footer from "./Footer/Footer"
 import Container from "./Container/Container"
 import Header from "./Header/Header"
-import { ListProvider } from "../context/ListContext"
+
 
 
 
 function Main() {
 
-    //const [toDos,setToDos] = useState(
-      //  JSON.parse(localStorage.getItem("key") || [] ));
+    const [toDos,setToDos] = useState(
+    JSON.parse(localStorage.getItem("key") || [] ));
 
     return (
         <div>
-        <ListProvider> 
+    
             <Header> </Header>
-            <Container> ds </Container>
+            <Container> </Container>
             <Footer> </Footer>
-        </ListProvider>
+        
         </div>
     )
 }
